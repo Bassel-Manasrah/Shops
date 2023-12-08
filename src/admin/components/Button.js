312eeeee7bb86cbe71d23f6edb8671edb624c1bf
Bassel Manasrah <bassel.manasrah@gmail.com>
2023-12-08 19:20:47 +0200

    added export to excel in orders page and users page

--- src/admin/components/Button.js
+++ src/admin/components/Button.js
@@ -1,14 +1,25 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Button = styled.button`
   background-color: ${(props) => props.color || "#007bff"};
   color: #fff;
-  padding: 10px 20px;
+  padding: ${(props) => props.padding || "8px 16px"};
   border: none;
   cursor: pointer;
-  &:hover {
-    transform: scale(1.05);
-  }
+  /* &:hover {
+    transform: scale(1.0);
+  } */
+  ${(props) =>
+    props.width &&
+    css`
+      width: ${props.width};
+    `}
+
+  ${(props) =>
+    props.height &&
+    css`
+      height: ${props.height};
+    `}
 `;
 
 export default Button;
